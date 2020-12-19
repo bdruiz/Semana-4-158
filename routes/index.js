9cb8d73f842c4353afbefcc5db69329f3430e6b7
andrehobbes <andrehobbes@gmail.com>
2020-12-18 22:21:51 -0500

    controlador de categorias

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const routerx = require('express-promise-router');
 const articuloRouter = require('./api/articulo.js');
 const categoriaRouter = require('./api/categoria.js');
 
-
 const router = routerx();
+router.use('/categoria',categoriaRouter);
 
 module.exports = router;
