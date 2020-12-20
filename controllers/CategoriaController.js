1acf66734c0a0eef43e239b4ba8151532fdab3cc
bdruiz <bdruizr@correo.udistrital.edu.co>
2020-12-19 22:06:51 -0500

    Se añade controldores de Articulo y usuario, config API y token

--- controllers/CategoriaController.js
+++ controllers/CategoriaController.js
@@ -21,9 +21,9 @@ exports.add = async (req, res, next) => {
 exports.update = async (req, res, next) => {
     try {
         const reg = await db.Categoria.update({
-            nombre: req.body.nombre, descripcion:
-                req.body.descripcion
-        }, { where: { id: req.body._id } });
+            nombre: req.body.nombre, 
+            descripcion: req.body.descripcion
+        }, { where: { id: req.body.id } });
         res.status(200).json(reg);
     } catch (e) {
         res.status(500).send({
@@ -36,7 +36,7 @@ exports.update = async (req, res, next) => {
 exports.activate = async (req, res, next) => {
     try {
         console.log(req.body._id);
-        const reg = await db.Categoria.update({ estado: 1 }, { where: { id: req.body._id } });
+        const reg = await db.Categoria.update({ estado: 1 }, { where: { id: req.body.id } });
         res.status(200).json(reg);
     } catch (e) {
         res.status(500).send({
@@ -48,7 +48,7 @@ exports.activate = async (req, res, next) => {
 
 exports.deactivate = async (req, res, next) => {
     try {
-        const reg = await db.Categoria.update({ estado: 0 }, { where: { id: req.body._id } });
+        const reg = await db.Categoria.update({ estado: 0 }, { where: { id: req.body.id } });
         res.status(200).json(reg);
     } catch (e) {
         res.status(500).send({
--- routes/api/articulo.js
+++ routes/api/articulo.js
@@ -1,12 +1,19 @@
 /* un Ejemplo  de como se veria la ruta listar - modelo del  articulo*/
 const routerx = require('express-promise-router');
-//const articuloController = require('../../controllers/ArticuloController.js');
+const articuloController = require('../../controllers/ArticuloController.js');
 const auth = require('../../middlewares/auth.js');
 
 const router = routerx();
 
 
-//router.get('/list', articuloController.list);
+router.get('/list', articuloController.list);
+router.post('/add', auth.verifyUsuario, articuloController.add);
+router.put('/update', auth.verifyUsuario, articuloController.update);
+router.put('/activate', auth.verifyUsuario, articuloController.activate);
+router.put('/deactivate', auth.verifyUsuario, articuloController.deactivate);
 
 
 module.exports = router;
+
+
+
